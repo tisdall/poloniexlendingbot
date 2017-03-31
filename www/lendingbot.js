1a58be04f2f18ae5eff09a3d9365a89133ba6549
Eric Carmichael <eric@ckcollab.com>
2017-03-31 15:15:19 -0700

    Display USD with 2 decimal places

--- www/lendingbot.js
+++ www/lendingbot.js
@@ -247,6 +247,8 @@ function Timespan(name, multiplier) {
             }
             if (currency == "BTC") {
                 return displayUnit.formatValue(earnings) + " <span class=" + currencyClass + ">" + displayUnit.name + "</span> / " + name + "<br/>"
+            } else if (currency == "USD" || currency == "USDT") {
+                return printFloat(earnings, 2) + " <span class=" + currencyClass + ">" + currency + "</span> / "+  name + "<br/>";
             } else {
                 return printFloat(earnings, 8) + " <span class=" + currencyClass + ">" + currency + "</span> / "+  name + "<br/>";
             }
