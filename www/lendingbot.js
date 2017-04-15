851c30e5c14d92f08f1c2e62823674de2582b61a
Raanan <rnevet@gmail.com>
2017-04-15 13:21:04 +0200

    Fix indentation issues Replace printFloat with prettyfloat for Fiat currencies (doesn't display .00)

--- www/lendingbot.js
+++ www/lendingbot.js
@@ -253,7 +253,7 @@ function Timespan(name, multiplier) {
             if (currency == "BTC") {
                 return displayUnit.formatValue(earnings) + " <span class=" + currencyClass + ">" + displayUnit.name + "</span> / " + name + "<br/>"
             } else if (currency == "USD" || currency == "USDT" || currency == "EUR") {
-                return printFloat(earnings, 2) + " <span class=" + currencyClass + ">" + currency + "</span> / "+  name + "<br/>";
+                return prettyFloat(earnings, 2) + " <span class=" + currencyClass + ">" + currency + "</span> / "+  name + "<br/>";
             } else {
                 return printFloat(earnings, 8) + " <span class=" + currencyClass + ">" + currency + "</span> / "+  name + "<br/>";
             }
